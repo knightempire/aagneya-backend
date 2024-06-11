484133083897667a6d9d4c5d4dfdfefd2a12f412
Abi <knightempire24@gmail.com>
2024-06-11 17:52:06 +0530

    app js is ready

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+require('dotenv').config();
 const {
     DB_HOST,
     DB_USER,
